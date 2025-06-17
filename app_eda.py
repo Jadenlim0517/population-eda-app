9bd67a248ec550ec1678cbc8f53e85efb8fddf45
Jadenlim0517 <ts24091@ts.hs.kr>
2025-06-17 05:05:24 +0000

    appeda2nd

--- app_eda.py
+++ app_eda.py
@@ -41,7 +41,7 @@ if "logged_in" not in st.session_state:
 # 홈 페이지 클래스
 # ---------------------
 class Home:
-    def __init__(self):
+    def __init__(self, login_page, register_page, findpw_page):
         st.title("🏠 Home")
         if st.session_state.get("logged_in"):
             st.success(f"{st.session_state.get('user_email')}님 환영합니다.")
